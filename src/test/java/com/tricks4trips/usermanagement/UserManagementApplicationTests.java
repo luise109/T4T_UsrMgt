a900f5e7520d2d3c5b0b2c78e9b6e64ecb3d75c7
Luis Eduardo CABRERA DEHESA <193220@ids.upchiapas.edu.mx>
2022-10-25 13:16:25 -0500

    first commit

---
+++ src/test/java/com/tricks4trips/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tricks4trips.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
